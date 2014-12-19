21f4787c5f28dd6cf4a7b2d9636f2809df39888a
Camila Arenas <mcarenash@Camilas-MacBook-Air.local>
2014-12-18 17:12:50 -0800

    Update

--- db.js
+++ db.js
@@ -3,14 +3,12 @@ var pg = require('pg');
 // Set up database
 var db = {};
 
-db.config = {
-  database: "boardfinder",
-  port: 5432,
-  host: "localhost"
-};
+db.config = {};
 
 db.connect = function(runAfterConnecting) {
-  pg.connect(db.config, function(err, client, done){
+  console.log(process.env.DATABASE_URL);
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
     if (err) {
       console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
     }
@@ -18,6 +16,20 @@ db.connect = function(runAfterConnecting) {
     done();
   });
 };
+  // database: "boardfinder",
+  // port: 5432,
+  // host: "localhost"
+// };
+
+// db.connect = function(runAfterConnecting) {
+//   pg.connect(db.config, function(err, client, done){
+//     if (err) {
+//       console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
+//     }
+//     runAfterConnecting(client);
+//     done();
+//   });
+// };
 
 db.query = function(statement, params, callback){
   db.connect(function(client){
