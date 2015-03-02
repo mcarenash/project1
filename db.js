a5b90abd9c511adadcdff2a25ee6fd74d524d635
mcarenash <mariacamilaarenasd@gmail.com>
2015-03-02 09:17:57 -0800

    rolledback

--- db.js
+++ db.js
@@ -3,7 +3,12 @@ var pg = require('pg');
 // Set up database
 var db = {};
 
-db.config = {}
+db.config = {
+  database: "boardfinder",
+  port: 5432,
+  host: "localhost"
+// };
+};
 
 // db.connect = function(runAfterConnecting) {
 //   console.log(process.env.DATABASE_URL);
@@ -17,10 +22,9 @@ db.config = {}
 //   });
 // };
 
-db.connect = function(runAfterConnecting) {
-  console.log(process.env.DATABASE_URL);
 
-  pg.connect(process.env.DATABASE_URL, function(err, client, done){
+db.connect = function(runAfterConnecting) {
+  pg.connect(db.config, function(err, client, done){
     if (err) {
       console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
     }
