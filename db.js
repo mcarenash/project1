e1a1eae7249154547b61897d538088c2dd992b73
mcarenash <mariacamilaarenasd@gmail.com>
2015-03-02 09:07:47 -0800

    db.js modified

--- db.js
+++ db.js
@@ -3,12 +3,7 @@ var pg = require('pg');
 // Set up database
 var db = {};
 
-db.config = {
-  database: "boardfinder",
-  port: 5432,
-  host: "localhost"
-// };
-};
+db.config = {}
 
 // db.connect = function(runAfterConnecting) {
 //   console.log(process.env.DATABASE_URL);
@@ -22,9 +17,10 @@ db.config = {
 //   });
 // };
 
-
 db.connect = function(runAfterConnecting) {
-  pg.connect(db.config, function(err, client, done){
+  console.log(process.env.DATABASE_URL);
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done){
     if (err) {
       console.error("OOOPS!!! SOMETHING WENT WRONG!", err);
     }
